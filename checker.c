6c8d7b983c81e6ff7a8692eebc56eb8a10359c2e
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-06-26 10:40:21 +0530

    Update checker.c commit3

--- checker.c
+++ checker.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <assert.h>
 int isTemperatureOutOfRange(float temperature) {
-    return (temperature < 0 || temperature > 45);
+  return (temperature < 0 || temperature > 45);
 }
-int isSOCOutOfRange(float soc) {
-    return (soc < 20 || soc > 80);
+int isSocOutOfRange(float soc) {
+  return (soc < 20 || soc > 80);
 }
 int isChargeRateOutOfRange(float chargeRate) {
-    return (chargeRate > 0.8);
+  return (chargeRate > 0.8);
 }
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    if (isTemperatureOutOfRange(temperature)) {
-        printf("Temperature out of range!\n");
-        return 0;
-    }
-    if (isSOCOutOfRange(soc)) {
-        printf("State of Charge out of range!\n");
-        return 0;
-    }
-    if (isChargeRateOutOfRange(chargeRate)) {
-        printf("Charge Rate out of range!\n");
-        return 0;
-    }
-    return 1;
+  if (isTemperatureOutOfRange(temperature)) {
+    return 0;
+  }
+  if (isSocOutOfRange(soc)) {
+    return 0;
+  }
+  if (isChargeRateOutOfRange(chargeRate)) {
+    return 0;
+  }
+  return 1;
 }
+ 
 int main() {
-    assert(batteryIsOk(25, 70, 0.7));
-    assert(!batteryIsOk(50, 85, 0));
+  float temperature1 = 25, soc1 = 70, chargeRate1 = 0.7;
+  float temperature2 = 50, soc2 = 85, chargeRate2 = 0;
+  // Check battery is ok for the first set of parameters
+  if (!batteryIsOk(temperature1, soc1, chargeRate1)) {
+    printf("Battery parameters out of range!\n");
+  }
+  // Check battery is ok for the second set of parameters
+  if (!batteryIsOk(temperature2, soc2, chargeRate2)) {
+    printf("Battery parameters out of range!\n");
+  }
+  // Assertions for automated testing
+  assert(batteryIsOk(temperature1, soc1, chargeRate1));
+  assert(!batteryIsOk(temperature2, soc2, chargeRate2));
+  return 0;
 }
