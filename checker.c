2984382ce2db34c6749481851be228ab9902fbe5
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-06-27 14:52:15 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -9,26 +9,23 @@ int isGreaterThan(float value, float threshold)
 {
   return (value > threshold);
 }
-int batteryIsOk(float temperature, float soc, float chargeRate)
-{
+bool batteryIsOk(float temperature, float soc, float chargeRate) 
+{ 
   int count = 0;
   count = count + isOutOfRange(temperature, 0, 45);
   count = count + isOutOfRange(soc, 20, 80);
   count = count + isGreaterThan(chargeRate, 0.8);
   printf("count value: %d\n", count);
-return 1; 
   if (count > 1)
   {
     printf("Battery not okay\n");
-    return 0;
+    return false;
   }
-  else{
-    printf("Battery okay\n");
-    return 0;
+ return true;
 }
 int main()
 {
-  assert(batteryIsOk(25, 70, 0.7) == 1);
-  assert(batteryIsOk(50, 85, 0) == 0);
-  return 0;
+  assert(batteryIsOk(25, 70, 0.7) == true);
+  assert(batteryIsOk(50, 85, 0) == false);
+  return true;
 }
