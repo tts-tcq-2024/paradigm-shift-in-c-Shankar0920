b27420b23519fb4b1d3c85155805cf956b5cf2ed
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-06-27 15:18:06 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if (temperature >= 0 && temperature <= 45 && soc >= 20 && soc <= 80 && chargeRate <= 0.8) {
-    return 1;
-  }
-  
-  printf("Battery parameters out of range!\n");
-  return 0;
+int isOutOfRange(float value, float lowerBound, float upperBound)
+{
+  return (value < lowerBound || value > upperBound);
 }
-
-int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+int isGreaterThan(float value, float threshold)
+{
+  return (value > threshold);
+}
+bool batteryIsOk(float temperature, float soc, float chargeRate) 
+{ 
+  int count = 0;
+  count = count + isOutOfRange(temperature, 0, 45);
+  count = count + isOutOfRange(soc, 20, 80);
+  count = count + isGreaterThan(chargeRate, 0.8);
+  printf("count value: %d\n", count);
+  if (count > 1)
+  {
+    printf("Battery not okay\n");
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+ 
+}
+int main()
+{
+  assert(batteryIsOk(25, 70, 0.7) == true);
+  assert(batteryIsOk(50, 85, 0) == false);
 }
