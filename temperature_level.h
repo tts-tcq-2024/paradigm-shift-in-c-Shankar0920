d0102a58f24990ce4d8c6904a48d86c3c3274d2c
Shankar0920 <167970503+Shankar0920@users.noreply.github.com>
2024-08-06 00:07:44 +0530

    Create temperature_level.h

---
+++ temperature_level.h
@@ -0,0 +1,12 @@
+#ifndef TEMPERATURE_LEVEL_H
+#define TEMPERATURE_LEVEL_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int low_temp_breach();
+int low_temp_warning();
+int temp_normal();
+int high_temp_warning();
+int high_temp_breach();
+#endif
